fa0967ba8787d4d16f8830e9e699d1d5e5498251
MalikHesham <malikhesham@outlook.com>
2021-07-27 20:09:44 +0200

    clean up

--- csvHasher.py
+++ csvHasher.py
@@ -1,9 +1,8 @@
 import csv
 import hashlib
 
-csvList = []
-
 def csvExtractor(fileName):
+    csvList = []
     rowCounter = 0
     with open(fileName, 'r') as csvFile:
         reader = csv.reader(csvFile)
@@ -13,17 +12,18 @@ def csvExtractor(fileName):
             if rowCounter %2 != 0:
                 csvList.append(row[2])
         csvFile.close()
-# csvExtractor('testcase.csv') => 443dec3062d0286986e21dc0631734c9
-csvExtractor('annual-enterprise-survey-2020-financial-year-provisional-csv.csv') # 4DD595E104B509F219A56D3E52F409AC
+    return csvList
 
 def stringCombiner(strList):
     unifiedString = ''
-    for element in csvList:
+    for element in strList:
         unifiedString += str(element)
     return unifiedString
 
 def md5Hasher(inputString): return hashlib.md5(inputString.encode())
 
+# csvList = csvExtractor('testcase.csv') # 443DEC3062D0286986E21DC0631734C9
+csvList = csvExtractor('annual-enterprise-survey-2020-financial-year-provisional-csv.csv') # 4DD595E104B509F219A56D3E52F409AC
 unHashedString = stringCombiner(csvList)
 hashedString = md5Hasher(unHashedString).hexdigest().upper()
 print(hashedString)
